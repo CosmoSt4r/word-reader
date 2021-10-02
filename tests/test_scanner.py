b89ca80ad40cc0f4b3264e1fde87c7fa57c58687
Andrey Kalkutin <CosmoStar@protonmail.com>
2021-10-02 21:08:19 +0500

    Tests rewritten with unittest

--- tests/test_scanner.py
+++ tests/test_scanner.py
@@ -1,29 +1,35 @@
-import pytest
+import unittest
 
 from wordreader.scanner import find_in_files
 
 docx = 'tests/docx_test.docx'
 doc = 'tests/doc_test.doc'
 
-def test_basic_cases():
-    assert find_in_files('', []) == {}
-    assert find_in_files('', [1, 2, 3]) == {'1': [], '2': [], '3': []}
-    assert find_in_files('1', [doc]) == find_in_files(1, [doc])
 
+class ScannerTest(unittest.TestCase):
+    def test_basic_cases(self):
+        self.assertEqual(find_in_files('', []), {})
+        self.assertEqual(
+                find_in_files('', [1, 2, 3]), {'1': [], '2': [], '3': []}
+            )
+        self.assertEqual(find_in_files('1', [doc]), find_in_files(1, [doc]))
 
-def test_different_formats():
+    def test_different_formats(self):
+        target = {
+            doc: ['Какой-то текст', 'Еще немного текста'],
+            docx: ['Какой-то текст', 'Еще немного текста'],
+        }
+        self.assertEqual(find_in_files('текст', [doc, docx]), target)
 
-    target = {
-        doc: ['Какой-то текст', 'Еще немного текста'],
-        docx: ['Какой-то текст', 'Еще немного текста'],
-    }
-    assert find_in_files('текст', [doc, docx]) == target
+    def test_case_sensitive_search(self):
+        target = {
+            doc: ['Еще немного текста'],
+            docx: ['Еще немного текста'],
+        }
+        self.assertEqual(find_in_files('еще', [doc, docx]), {doc: [], docx: []})
+        self.assertEqual(
+                find_in_files('еще', [doc, docx], case_sensitive=False), target
+            )
 
-
-def test_case_sensitive_search():
-    target = {
-        doc: ['Еще немного текста'],
-        docx: ['Еще немного текста'],
-    }
-    assert find_in_files('еще', [doc, docx]) == {doc: [], docx: []}
-    assert find_in_files('еще', [doc, docx], case_sensitive=False) == target
+if __name__ == '__main__':
+    unittest.main()
